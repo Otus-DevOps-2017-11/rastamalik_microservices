81d7d991e0f9a56dab7bc87ec1692b7e81f3fecd
Administrator <admin@example.com>
2018-02-20 06:35:07 +0000

    Add new file

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable zone {
+  description = "Zone"
+}
+
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable private_key {
+  description = "private ssh key"
+}
