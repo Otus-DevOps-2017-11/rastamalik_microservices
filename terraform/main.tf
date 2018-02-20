5ad7529d1d173f45af03a3031877429c663324e8
Administrator <admin@example.com>
2018-02-20 06:16:12 +0000

    Add new file

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+provider "google" {
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
+
+}
+
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  metadata {
+    sshKeys = "appuser:${file(var.public_key_path)} "
+    
+ }
+
+
+
+  tags = ["reddit-app"]
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key)}"
+  }
+
