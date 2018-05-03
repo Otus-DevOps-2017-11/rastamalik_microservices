a82c5e0ecdf9279de98feceadccd1b6b2c5dfb52
Mozhaev Alexander <rastamalik@gmail.com>
2018-05-03 09:17:34 +0300

    Add kubernetes 5

---
+++ kubernetes/ansible/template/pod_networks.sh
@@ -0,0 +1,10 @@
+for instance in worker-0 worker-1 worker-2; do
+  gcloud compute instances describe ${instance} \
+    --format 'value[separator=" "](networkInterfaces[0].networkIP,metadata.items[0].value)'
+done
+for i in 2 3 4; do
+  gcloud compute routes create kubernetes-route-10-200-${i}-0-24 \
+    --network kubernetes-the-hard-way \
+    --next-hop-address 10.240.0.${i} \
+    --destination-range 10.200.0.0/24
+done
